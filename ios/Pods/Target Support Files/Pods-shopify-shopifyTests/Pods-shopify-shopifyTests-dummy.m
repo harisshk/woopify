7041cd1640a605f3cb5530fe8ff24a8210710b23
Hk <hk@Hks-MacBook-Pro.local>
2021-12-09 10:43:12 +0530

    new temp

--- ios/Pods/Target Support Files/Pods-shopify-shopifyTests/Pods-shopify-shopifyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_shopify_shopifyTests : NSObject
-@end
-@implementation PodsDummy_Pods_shopify_shopifyTests
-@end
---
+++ template.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    placeholderName: "shopify",
+    templateDir: "./template",
+}
